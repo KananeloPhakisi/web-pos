dd628a7746bd7ebd86a95cf84f7fc937d730d497
KananeloPhakisi <phakisikananelo0@gmail.com>
2020-12-11 20:45:57 +0200

    Write server

--- web-pos/server.js
+++ web-pos/server.js
@@ -1,12 +1,32 @@
+'use-strict';
+
 const app = require("./app");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const logger = require('morgan');
 const dotenv = require("dotenv");
 dotenv.config();
 
+const Router = reqiure('./Backend/Routes/index');
+
+const app = express();
+const nodeDev = 'Development';
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false;
+}));
+app.use('/', Router);
 
 app.listen(port, error => {
 	if (error) {
-		console.log(`Error: `)
+		console.log(`Error: ${error}`);
 	}
 	console.log(`Server running on port ${port}...`);
-})
+});
+
+module.exports = app;
