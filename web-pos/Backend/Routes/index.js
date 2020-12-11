407ba86582e0d48a13f901102da839eb480ef5f0
KananeloPhakisi <phakisikananelo0@gmail.com>
2020-12-11 22:15:17 +0200

    Add routes

---
+++ web-pos/Backend/Routes/index.js
@@ -0,0 +1,23 @@
+'use-strict';
+
+const express = require('express'),
+    product = require('./product'),
+    category = require('./category'),
+    order = require('./order'),
+    user = require('./user'),
+    {validateUser} = require('../Helpers/middleware');
+
+const Router = express.Router();
+
+Router.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to web based Point Of Sales."
+    });
+})
+
+Router.use('/product', validateUser, product);
+Router.use('/category', validateUser, category);
+Router.use('/order', validateUser, order);
+Router.use('/user', user);
+
+module.exports = Router;
